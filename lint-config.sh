ccd172d1401b1e259f75cc87180ea08f6b6f2732
Anthony <99536297+anthony-spruyt@users.noreply.github.com>
2026-08-11 22:33:14 +1000

    Update lint-config.sh
    Signed-off-by: Anthony <99536297+anthony-spruyt@users.noreply.github.com>

--- lint-config.sh
+++ lint-config.sh
@@ -5,7 +5,7 @@
 
 # MegaLinter Docker image (use digest for reproducibility)
 # renovate: datasource=docker depName=ghcr.io/anthony-spruyt/megalinter-container-images
-MEGALINTER_IMAGE="ghcr.io/anthony-spruyt/megalinter-container-images:v10.0.34@sha256:c5b536dac1b500804e42beb3069019e6c823b0ee140952179d473fbae37e7005"
+MEGALINTER_IMAGE="ghcr.io/anthony-spruyt/megalinter-container-images:latest"
 
 # Skip linting for renovate/dependabot commits in CI
 SKIP_BOT_COMMITS=false
